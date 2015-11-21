e81affefaf2b4976576759e3bc608973c90264a9
SweetyGott <jens.woehrle@tum.de>
2015-11-21 17:09:12 +0100

    SetupWebSite working

--- node-postgres/models/database.js
+++
@@ -1,8 +0,0 @@
-
-var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/db_proj1';
-
-var client = new pg.Client(connectionString);
-client.connect();
-var query = client.query('select * from db.sitzeproland2013');
-query.on('end', function() { client.end(); });
