39867e3aea5675e6dabd163796114f3f88db70ad
SweetyGott <jens.woehrle@tum.de>
2015-12-06 18:21:05 +0100

    jetzt aber

--- node-postgres/client/public/javascripts/app.js
+++ node-postgres/client/public/javascripts/app.js
@@ -446,7 +446,7 @@ angular.module('nodeTodo', ['googlechart', 'ngRoute', 'ngTable', 'ui.router'])
                 $scope.startKnappsteSieger();                
                 break;
             case "#/justbundestag":
-                $scope.startKnappsteSieger();                
+                $scope.startJustBundestag();                
                 break;
             default:
                 $scope.startMain();
