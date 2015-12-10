55563e5dbd86e9140b6a06980f492d0291e66961
Martin Wurzer <martin.wrzr@gmail.com>
2015-12-10 16:42:24 +0100

    added token generator, DB in dropbox

---
+++ SQLAbfragen/sql/generateTokens.sql
@@ -0,0 +1,24 @@
+﻿
+CREATE OR REPLACE FUNCTION public.generateTokens () RETURNS void AS
+$$
+
+	DECLARE 
+		anz_wahlber integer;
+		i integer;
+
+	BEGIN
+
+	BEGIN
+        FOR i IN 1..299
+        LOOP
+
+        SELECT wähler13 INTO anz_wahlber FROM wahlkreise WHERE id = i;
+
+        INSERT INTO token (wk_id, token_id, used) SELECT i, generate_series, False FROM generate_series(1, anz_wahlber) series;
+	
+	END LOOP;
+	COMMIT;
+   	END;
+   	
+$$
+LANGUAGE plpgsql VOLATILE
